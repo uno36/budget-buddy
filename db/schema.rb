49912c507d131897c51a4840befab9442ea635b4
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-09-20 14:46:01 +0200

    Add model transaction

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_20_114823) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_20_122340) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +61,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_20_114823) do
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.string "icon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "transactions", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
