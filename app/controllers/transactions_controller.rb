7a59f9dbfa66f8b0c5199e9e498ed749ef240772
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-09-22 00:01:05 +0200

    Factor the code and correct Linter errors

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,26 +1,21 @@
 class TransactionsController < ApplicationController
   def index
-    @category = Category.find_by(id: params[:category_id])
-
-    if @category
-      @transactions = @category.transactions.order(created_at: :desc)
-      @total_amount = @category.transactions.sum(:amount)
-    else
-      # Handle the case when the category is not found
-      flash[:alert] = 'Category not found'
-      redirect_to categories_path # You can redirect to a suitable page
-    end
+    @category = Category.find(params[:category_id])
+    @transactions = @category.transactions.order(created_at: :desc)
+    @total_amount = @category.transactions.sum(:amount)
   end
 
   def new
     @transaction = Transaction.new
     @category = Category.find(params[:category_id])
     @transaction = @category.transactions.new
+    @transaction = @category.transactions.build
   end
 
   def create
     @category = Category.find(params[:category_id])
     @transaction = @category.transactions.new(transaction_params)
+    @transaction = @category.transactions.build(transaction_params)
     if @transaction.save
       redirect_to category_transactions_path(@category)
     else
