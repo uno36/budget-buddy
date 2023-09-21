12a67e60b23ae3e99667b4130f39a2cf0ff5838b
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-09-21 15:52:51 +0200

    Make migrations

--- db/migrate/20230920122340_create_transactions.rb
+++ db/migrate/20230920122340_create_transactions.rb
@@ -1,7 +1,8 @@
 class CreateTransactions < ActiveRecord::Migration[7.0]
   def change
     create_table :transactions do |t|
-
+      t.references :category, foreign_key: true 
+      
       t.timestamps
     end
   end
